002d0ac65d51a8652170ed4088da09c6d0107067
nyinyixaw <nyinyizaw105202@gmail.com>
2020-12-08 20:22:30 +0630

    facebook

---
+++ js/script.js
@@ -0,0 +1,6 @@
+    
+    
+<form class="form-inline">
+       <input type="email" name="#"  style="margin-left:90px">&nbsp;
+       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+     </form>
